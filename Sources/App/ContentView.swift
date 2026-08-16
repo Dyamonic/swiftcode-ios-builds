aba29cc5040a1dc1394e0bb79bd302ec41aef2ae
Dyamonic <ihoundaq@gmail.com>
2026-08-16 05:44:55 +0300

    SwiftCode update Sources/App/ContentView.swift

--- Sources/App/ContentView.swift
+++ Sources/App/ContentView.swift
@@ -63,6 +63,8 @@ struct ContentView: View {
             }
             .tag(2)
         }
+        .preferredColorScheme(.dark)
+        .background(Color.black.ignoresSafeArea())
     }
 
     private var heroSection: some View {
